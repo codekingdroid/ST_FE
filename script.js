9f1e55868274f92fbd64fd127822b2c11a898239
codekingdroid <55647482+codekingdroid@users.noreply.github.com>
2021-01-16 13:54:10 +0530

    Added script.js
    Matches Username and Password!

---
+++ script.js
@@ -0,0 +1,7 @@
+function checkPassword() {
+    if (pswd.value == 'chirkut' && uname.value == 'chomu') {
+        window.open("home.html");
+    } else {
+        alert("Try again, it's easy!");
+    }
+}
